4b223af0bf8cefb669ced7d1c04160fa19080b1d
Sean Hastings <shastings@instructionalempowerment.com>
2023-04-21 15:30:34 -0400

    Attempt to get version 7 working on Grails 3.3.16 - currently broken

--- grails-plugin/grails-app/init/grails/plugins/mongodb/Application.groovy
+++ grails-plugin/grails-app/init/grails/plugins/mongodb/Application.groovy
@@ -1,14 +1,15 @@
 package grails.plugins.mongodb
 
-import grails.boot.GrailsApp
+//import grails.boot.GrailsApp
 import grails.plugins.metadata.PluginSource
 import groovy.transform.CompileStatic
+import org.springframework.context.ConfigurableApplicationContext
 
 @CompileStatic
 @PluginSource
 class Application {
 
     static void main(String[] args) {
-        GrailsApp.run(Application, args)
+        ConfigurableApplicationContext result = grails.boot.GrailsApp.run(Application as Class<Application>, args)
     }
 }
